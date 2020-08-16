a15e584df91e9a8a1310c93ed691b8366886c6da
Sid Price <sid@sidprice.com>
2020-08-16 16:57:59 -0600

    Extract test packets to class level
    They may then be used for other tests

--- test_probe.py
+++ test_probe.py
@@ -8,13 +8,19 @@ import probe as Probe
 
 class TestProbe(unittest.TestCase):
     '''
+        Test packets
+    '''
+    _pkt8 = '$38#6B'  # 8-bit value
+    _pkt16 = '$3038#CE'  # 16-bit value
+    _pkt32 = '$00380240#91'  # 32-bit value
+    _pkt64 = '$0038024010000000#12'  # 64-bit value
+
+    '''
         Test that "_extractReply" correctly extracts 8, 16, and 32 bit values
 
         Note that the method "_extractReply" assumes the calling
         method has validated the packet; it does no validation of
         the packet.
-
-        TODO Do we need 64 bit values?
     '''
 
     '''
@@ -23,14 +29,12 @@ class TestProbe(unittest.TestCase):
     '''
 
     def test_extractReply_8bit_success(self):
-        pkt8 = '$38#6B'  # 8-bit value
         probe = Probe.Probe(None)
-        self.assertEqual(probe._extractReply(pkt8), '38')
+        self.assertEqual(probe._extractReply(self._pkt8), '38')
 
     def test_extractReply_8bit_fail(self):
-        pkt8 = '$38#6B'  # 8-bit value
         probe = Probe.Probe(None)
-        self.assertNotEqual(probe._extractReply(pkt8), '00')
+        self.assertNotEqual(probe._extractReply(self._pkt8), '00')
 
     '''
         Tests
@@ -38,24 +42,48 @@ class TestProbe(unittest.TestCase):
     '''
 
     def test_extractReply_not_8bit(self):
-        pkt16 = '$3038#CE'  # 8-bit value
         probe = Probe.Probe(None)
-        self.assertNotEqual(probe._extractReply(pkt16), '30')
-        self.assertNotEqual(probe._extractReply(pkt16), '38')
+        self.assertNotEqual(probe._extractReply(self._pkt16), '30')
+        self.assertNotEqual(probe._extractReply(self._pkt16), '38')
 
     '''
         Tests:
             Good and bad 16-bit results
     '''
+
     def test_extractReply_16bit_success(self):
-        pkt16 = '$3038#CE'  # 8-bit value
         probe = Probe.Probe(None)
-        self.assertEqual(probe._extractReply(pkt16), '3038')
+        self.assertEqual(probe._extractReply(self._pkt16), '3038')
 
     def test_extractReply_16bit_fail(self):
-        pkt16 = '$3038#CE'  # 8-bit value
         probe = Probe.Probe(None)
-        self.assertNotEqual(probe._extractReply(pkt16), '0000')
+        self.assertNotEqual(probe._extractReply(self._pkt16), '0000')
+
+    '''
+        Tests:
+            Good and bad 32-bit results
+    '''
+
+    def test_extractReply_32bit_success(self):
+        probe = Probe.Probe(None)
+        self.assertEqual(probe._extractReply(self._pkt32), '00380240')
+
+    def test_extractReply_32bit_fail(self):
+        probe = Probe.Probe(None)
+        self.assertNotEqual(probe._extractReply(self._pkt32), '00000000')
+
+    '''
+        Tests:
+            Good and bad 64-bit results
+    '''
+
+    def test_extractReply_64bit_success(self):
+        probe = Probe.Probe(None)
+        self.assertEqual(probe._extractReply(self._pkt64), '0038024010000000')
+
+    def test_extractReply_64bit_fail(self):
+        probe = Probe.Probe(None)
+        self.assertNotEqual(probe._extractReply(self._pkt64), '0000000000000000')
 
 
 if __name__ == '__main__':
