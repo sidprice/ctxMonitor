5f2050e3bb016c67e34b854dd93de709e95e6bfb
Sid Price <sid@sidprice.com>
2020-08-17 17:08:10 -0600

    Add support for converting up to 64-bit values from ascii hex string

--- utilities.py
+++ utilities.py
@@ -3,19 +3,34 @@ def integerFromAsciiHex(asciihex):
         Convert the ASCII-HEX string parameter to
         an integer value.
 
+        This function processes 8, 16, 32, and 64-bit strings. The
+        formats are:
+            8-bit                 zz
+            16-bit              yyzz
+            32-bit          wwxxyyzz
+            64-bit  ssttuuvvwwxxyyzz
+        
         This method is specifically for converting 32-bit
-        values from the ctxLink debug probe. The format is:
-            zzyyxxww
-        Where 'ww' is the most significant byte and 'zz'
+
+        Where 'ss' is the most significant byte and 'zz'
         is the least significant
 
         If the input is less than the required length or
-        contaions non-hex digits, return 'None'
+        contains non-hex digits, return 'None'
     '''
     #
-    # first correct the byte order to wwxxyyzz
+    # first correct the byte order to ssttuuvvwwxxyyzz
     #
-    value = asciihex[6:] + asciihex[4:6] + asciihex[2:4] + asciihex[:2]
+    length = len(asciihex)
+    if (length > 0 and length < 4):
+        value = asciihex[:2]
+    elif (length < 8):
+        value = asciihex[2:4] + asciihex[:2]
+    elif (length < 16):
+        value = asciihex[6:] + asciihex[4:6] + asciihex[2:4] + asciihex[:2]
+    else:
+        value = asciihex[14:] + asciihex[12:14] + asciihex[10:12] + asciihex[8:10]  + asciihex[6:8] + asciihex[4:6] + asciihex[2:4] + asciihex[:2]
+
     #
     # Iterate over the string of hex characters and
     # convert to integer
@@ -29,3 +44,6 @@ def integerFromAsciiHex(asciihex):
 
 def integerToHexDisplayValue(integer):
     return hex(integer)
+
+if __name__ == '__main__':
+    print(integerFromAsciiHex(''))
