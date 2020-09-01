b677f55fef3bacf6d4f74cff16c717bb68880687
Sid Price <sid@sidprice.com>
2020-09-01 15:56:09 -0600

    Convert Ctx_PubSub to a Singleton class

--- ctx_pubsub.py
+++ ctx_pubsub.py
@@ -3,6 +3,10 @@
 #   A wrapper class for the pypubsub extension providng any required extras
 #   for the pubsub schema
 #
+#   NOTE:
+#       This is a Singleton class, the whole application must use the
+#       same instance.
+#
 ##########################################################################
 
 from pubsub import pub
@@ -16,11 +20,11 @@ from pubsub import pub
 ###
 #
 #   Load the requested ELF file:
-#       The Main Window of the UI sends this request. 
+#       The Main Window of the UI sends this request.
 #
 #       The Variable Manager listens to this topic, it loads the file and
 #       creates the database of variables defined in the file.
-TOPIC_ELF_FILE_LOAD  = 'root.load_elf_file'
+TOPIC_ELF_FILE_LOAD = 'root.load_elf_file'
 
 ###
 #
@@ -32,18 +36,34 @@ TOPIC_ELF_FILE_LOAD  = 'root.load_elf_file'
 TOPIC_VARIABLE_DB = 'root.variable_database'
 
 
-
 class Ctx_PubSub():
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        '''
+            Static access method
+        '''
+        if Ctx_PubSub.__instance == None:
+            Ctx_PubSub()
+        return Ctx_PubSub.__instance
+
     def __init__(self):
-        pass
-    
+        '''
+            Virtually private constructor
+        '''
+        if Ctx_PubSub.__instance != None:
+            raise Exception('This class is a singleton')
+        else:
+            Ctx_PubSub.__instance = self
+
     ###
     #
     #   Send message to request loading the given elf file. Filename
     #   must be fully-qualified path/name
     #
     def send_load_elf_file(self, elf_filename):
-        pub.sendMessage(TOPIC_ELF_FILE_LOAD, elf_file = elf_filename)
+        pub.sendMessage(TOPIC_ELF_FILE_LOAD, elf_file=elf_filename)
 
     ###
     #
@@ -57,8 +77,8 @@ class Ctx_PubSub():
     #   Send the given database topic to listeners
     #
     def send_variable_database(self, database):
-        pub.sendMessage(TOPIC_VARIABLE_DB, data = database)
-    
+        pub.sendMessage(TOPIC_VARIABLE_DB, data=database)
+
     ###
     #
     #   Subscribe to the variable database topic
@@ -66,17 +86,25 @@ class Ctx_PubSub():
     def subscribe_variable_database(self, listener):
         pub.subscribe(listener, TOPIC_VARIABLE_DB)
 
+
 def myElf_Listener(elf_file):
     print(elf_file)
+
 
 def myDatabase_Listener(data):
     print(data)
 
-if __name__ == '__main__':
-    pubsub_manager = Ctx_PubSub()
-    pubsub_manager.subscribe_load_elf_file(myElf_Listener)
-    pubsub_manager.subscribe_variable_database(myDatabase_Listener)
-    pubsub_manager.send_load_elf_file('this file')
-    somedata = { "one": "value_one", "two": "value_two"}
-    pubsub_manager.send_variable_database(somedata)
 
+if __name__ == '__main__':
+    # s = Ctx_PubSub()
+    # print(s)
+    s = Ctx_PubSub.getInstance()
+    print(s)
+    s = Ctx_PubSub.getInstance()
+    print(s)
+    # pubsub_manager = Ctx_PubSub()
+    # pubsub_manager.subscribe_load_elf_file(myElf_Listener)
+    # pubsub_manager.subscribe_variable_database(myDatabase_Listener)
+    # pubsub_manager.send_load_elf_file('this file')
+    # somedata = {"one": "value_one", "two": "value_two"}
+    # pubsub_manager.send_variable_database(somedata)
