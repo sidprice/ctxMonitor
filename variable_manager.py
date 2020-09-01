8e98a07e2715316c9ba69aaa2d288568990f7001
Sid Price <sid@sidprice.com>
2020-09-01 17:02:24 -0600

    Convert VariableManager to Singleton

--- variable_manager.py
+++ variable_manager.py
@@ -3,4 +3,40 @@
 #   This class loads the variable from the ELF file and serves any changes
 #   in the values of monitored variables to the rest of the system
 #
+#   NOTE:
+#       This is a Singleton class, the whole application must use the
+#       same instance.
+#
 ##########################################################################
+
+from ctx_pubsub import Ctx_PubSub
+
+
+class VariableManager():
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        '''
+            Static Access method
+        '''
+        if VariableManager.__instance == None:
+            VariableManager()
+        return VariableManager.__instance
+
+    def __init__(self):
+        '''
+            Virtually private constructor
+        '''
+        if VariableManager.__instance != None:
+            raise Exception('This class is a singleton')
+        else:
+            VariableManager.__instance = self
+            pubsub = Ctx_PubSub.getInstance()
+            #
+            # Subscribe to elf file loading requests
+            #
+            pubsub.subscribe_load_elf_file(_listener_elf_file)
+
+    def _listener_elf_file(self, filename):
+        print(filename)
