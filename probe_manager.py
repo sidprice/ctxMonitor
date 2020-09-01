4c59336ce20c17334a58e4a9bc9f596275390b96
Sid Price <sid@sidprice.com>
2020-09-01 14:45:12 -0600

    Skeleton files

---
+++ probe_manager.py
@@ -0,0 +1,6 @@
+##########################################################################
+#
+#   This class manages the connection with the probe and does the 
+#   variable monitoring as requested.
+#
+##########################################################################
---
+++ variable_manager.py
@@ -0,0 +1,6 @@
+##########################################################################
+#
+#   This class loads the variable from the ELF file and serves any changes
+#   in the values of monitored variables to the rest of the system
+#
+##########################################################################
