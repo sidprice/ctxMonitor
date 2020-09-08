fd72bd2bb94b2bdd8c130fa17474ff54be3a71f9
Sid Price <sid@sidprice.com>
2020-09-07 18:04:10 -0600

    Better layout for symbol selector

--- symbol_select_dialog.py
+++ symbol_select_dialog.py
@@ -19,9 +19,27 @@ class SelectSymbol(QtWidgets.QDialog):
 
         self.setWindowTitle('Select Symbol')
         #
+        #   Limit the size of the dialog
+        #
+        self.setMaximumWidth(450)
+        self.setMinimumWidth(450)
+        self.setMaximumHeight(300)
+        self.setMinimumHeight(300)
+        #
         #   Create the controls
         #
         self._symbols_view = QTableWidget()
+        self._symbols_view.setColumnCount(3)
+        self._symbols_view.setHorizontalHeaderItem(0, QTableWidgetItem('Name'))
+        self._symbols_view.setHorizontalHeaderItem(1, QTableWidgetItem('Address'))
+        self._symbols_view.setHorizontalHeaderItem(2, QTableWidgetItem('Update Period'))
+        self._symbols_view.setColumnWidth(0, self.width() / 3)
+        self._symbols_view.setColumnWidth(1, self.width() / 3)
+        # self._symbols_view.setColumnWidth(2, self.width() / 3)
+
+
+        self._symbols_view.horizontalHeader().setStretchLastSection(True)
+        self._symbols_view.verticalHeader().hide()
         self._ok_button = QPushButton('OK')
         self._ok_button.clicked.connect(self._okButtonPressed)
         self._cancel_button = QPushButton('Cancel')
@@ -54,7 +72,7 @@ class SelectSymbol(QtWidgets.QDialog):
         self.close()
 
     def _displaySymbols(self):
-        self._symbols_view.setColumnCount(2)
+        # self._symbols_view.setColumnCount(2)
         row = 0
         for name, value in self._symbols.items():
             self._symbols_view.setRowCount(row+1)
@@ -64,6 +82,9 @@ class SelectSymbol(QtWidgets.QDialog):
             item = QTableWidgetItem(f'0x{value}')
             item.setTextAlignment(Qt.AlignCenter)
             self._symbols_view.setItem(row, 1, item)
+            # item = QTableWidgetItem('0')
+            # item.setTextAlignment(Qt.AlignCenter)
+            # self._symbols_view.setItem(row, 2, item)
             row += 1
 
 if __name__ == '__main__':
