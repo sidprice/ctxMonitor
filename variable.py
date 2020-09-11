ed921b9ab23547e6f4d37954585b6dc405c6b670
Sid Price <sid@sidprice.com>
2020-09-11 08:10:28 -0600

    Add default, zero sample period to Variable class

--- variable.py
+++ variable.py
@@ -1,11 +1,11 @@
 ##########################################################################
 #
-#   Class for storing the variables enabled for monitoring
+#   Class for storing the variables and those variables enabled for monitoring
 #
 ##########################################################################
 
 class Variable(object):
-    def __init__(self, name, address, period):
+    def __init__(self, name, address, period=0,):
         super().__init__()
 
         self.name = name
