60e51d91ae5eaf73a5529767b99937c78047b1ec
Sid Price <sid@sidprice.com>
2020-09-14 15:14:10 -0600

    Added enable field to Variable class

--- variable.py
+++ variable.py
@@ -7,23 +7,26 @@
 from ctx_timing import CtxTiming
 import json
 
+
 class Variable:
-    def __init__(self, name, address, period=CtxTiming.Period_Default):
+    def __init__(self, name, address, period=CtxTiming.Period_Default, enable=True):
         super().__init__()
 
         self.name = name
         self.address = address
         self.period = period
+        self.enable = enable
 
-    @staticmethod    
+    @staticmethod
     def decode_variable(o):
         if Variable.__name__ in o:
-            return Variable(name=o['name'], address=o['address'], period=o['period'])
+            return Variable(name=o['name'], address=o['address'], period=o['period'], enable=o['enable'])
         return o
+
 
 class VariableEncoder(json.JSONEncoder):
     def default(self, o):
         if (isinstance(o, Variable)):
-            return {'name': o.name, 'address': o.address, 'period': o.period, o.__class__.__name__: True}
+            return {'name': o.name, 'address': o.address, 'period': o.period, 'enable': o.enable, o.__class__.__name__: True}
         else:
-            return json.JSONEncoder(self,0)
+            return json.JSONEncoder(self, 0)
